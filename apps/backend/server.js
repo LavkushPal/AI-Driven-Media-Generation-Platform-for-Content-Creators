fd2508dcdcf0a9a747533c96c2bdc8825ac271ec
LavkushPal <lavkushpal134@gmail.com>
2026-02-10 14:08:21 +0530

    v1.0.1-beta

--- apps/backend/server.js
+++ apps/backend/server.js
@@ -32,7 +32,7 @@ app.use(session({
     cookie: {
         maxAge: 1000 * 60 * 60 * 24,
         httpOnly: true,
-        secure: isProd,                 // true in prod (HTTPS)
+        secure: false,                 // true in prod (HTTPS)
         // sameSite: isProd ? "none" : "lax",
         sameSite: "lax",
     }
